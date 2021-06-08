e04c795bf282a9a2b256d8963827fb56a92906ad
hjchoi <hyecho.choi@gmail.com>
2021-06-08 09:29:26 +0900

    swiper 기능 추가

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-window.onload = function(){
-    const swiper = new Swiper(".mySwiper", {
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
-
-  
-    var start_btn = document.getElementsByClassName('start_btn')[0]
-
-    function click_start_btn(){
-        alert("do")
+const swiper = new Swiper(".swiper-container", {
+    effect: 'flip',
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     }
-
-
-}
+});
 
