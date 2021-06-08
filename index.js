519d2dfa125b502f066194e62b378afa1cd7d6b0
hjchoi <hyecho.choi@gmail.com>
2021-06-08 18:11:37 +0900

    json 변경 / title만 띄우기

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var navi = document.getElementsByTagName('nav')[0]
 var swiper = new Swiper('.main_box', {
     effect: 'flip',
-    allowTouchMove: true,
+    allowTouchMove: false,
     breakpointsInverse: true,
     breakpoints: {
         1023: {
@@ -38,15 +38,28 @@ request.send();
 // for문 돌면서 다 세팅할지 아니면 다이나믹하게 그때그때 생성? 
 request.onload = function() {
     var datalist = request.response;
-    var q1 = document.getElementsByClassName('q1')[0]
-    var title = q1.getElementsByClassName('q_title')[0]
-    title.innerText = datalist.q_list[0].q1.q_text
-    console.log(datalist)
-    a_list = datalist.q_list[0].q1.a_list
-    console.log(a_list)
-    var btn_list = q1.getElementsByClassName('select_btn_box')[0].getElementsByTagName('button')
-    for (let index = 0; index < btn_list.length; index++) {
-        btn_list[index].innerText = a_list[index].text
-        console.log(a_list[index])
-    }
+    load_text(datalist)
   }
+
+  function load_text(datalist) {
+    var q_class_list = document.getElementsByClassName('q')
+     for (let index = 0; index < q_class_list.length; index++) {
+        q_class = q_class_list[index]
+        title = q_class.getElementsByClassName('q_title')[0]
+        title.innerText = datalist.q_list[index].q_text
+        a_list = datalist.q_list[index].a_list
+        var btn_list = q_class.getElementsByClassName('select_btn_box')[0].getElementsByTagName('button')
+        console.log(btn_list)
+        // for (let j = 0; j < btn_list.length; index++) {
+        //     btn_list[j].innerText = a_list[j].text
+        // }
+     }
+  }
+
+  var next_btn = document.getElementsByClassName('swiper-button-next')[0]
+  var icon = document.getElementsByClassName('prog')[0].getElementsByTagName('img')
+  console.log(icon)
+  var left = icon.style.left
+  next_btn.addEventListener('click', function () {
+    icon.style.left = left -20
+})
